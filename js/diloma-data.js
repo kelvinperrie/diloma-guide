6cdc19ce3de7adc2b7e1c0bcbf3b7bec25cd84d8
kelvin <kelvinperrie@gmail.com>
2023-05-19 13:48:09 +1200

    a whole lot of ground-work

--- js/diloma-data.js
+++ js/diloma-data.js
@@ -1,23 +1,36 @@
+
+/*
+    details : [
+        { text: "colouring, marks" },
+        { text: "texture, spirals vs growth" },
+        { text: "habitat" },
+    ]
+*/
+
 var snailData = [
     {
         commonName : "Spotted Top Snail",
         scientificName : "Diloma aethiops",
         details : [
-            { text : "asdfasdfasdf", references : [1] }
+            { text : "Very common. Max height of 30mm.", references : [1] },
+            { text : "Generally not a deep black, more blackish purple. Usually has a chequered pattern of white.", references : [] },
+            { text : "Spiral lines and growth lines are both obvious and make a cross hatch pattern over a lot of the shell.", references : [] },
+            { text : "Found all through the intertidal zones on rocks and will be the only top snail on exposed rocks. Can also be found under rocks.", references : [1] },
 
         ],
         images : [],
         references : [
-            { id: 1, detail: "" }
+            { id: 1, detail: "Carson, Sally & Morris (2022) Rod The New Zealand Seashore Guide (pp165). Potton & Burton" }
         ]
     },
     {
         commonName : "Bluish Top Shell",
         scientificName : "Diloma nigerrimum",
         details : [
-            { text : "Spherical shaped. 18mm average, 25mm max", references : [1] },
-            { text : "The spiral lines going from front to back are often very faded. Growth lines running across the shell are often well developed. ", references : [2] }
-
+            { text : "Max height of 25mm", references : [1] },
+            { text : "A very dark blue, often hard to tell apart from black, especially in photos.", references : [] },
+            { text : "Spiral lines often very faded. Growth lines are well developed. ", references : [2] },
+            { text : "Found tidally where???. ", references : [] }
         ],
         images : [],
         references : [
